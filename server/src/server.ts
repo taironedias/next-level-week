88bfb36ca643481550e205b0eea2019874466f55
taironecdias <taironecdias@gmail.com>
2020-06-02 22:14:21 -0300

    separando os arquivos em rotas e controllers

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,15 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    res.send([
-        'Marcelo',
-        'Tairone',
-        'Maca',
-        'Ênio'
-    ]);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333, () => {
     console.log("server runing port 3333");
