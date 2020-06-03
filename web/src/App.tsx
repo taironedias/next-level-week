9e7e95316b3c4f83e70a081395136cf2508565eb
taironecdias <taironecdias@gmail.com>
2020-06-03 19:42:50 -0300

    limpando novamente o projeto

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import { useState } from 'react';
 import './App.css';
 
-import Header from './Header';
-
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title={ `Hello Tairone ${counter}` } />
-      <h1>Conteudo da aplicação</h1>
-      <p>{ counter }</p>
-      <p>{ counter * 2 }</p>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+    <h1>Conteudo da aplicação</h1>
   );
 }
 
