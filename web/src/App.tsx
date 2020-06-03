f39fe0e6e36642bb49b79a8abee38b107349d654
taironecdias <taironecdias@gmail.com>
2020-06-03 19:38:22 -0300

    adicionando o conceito de estado e imutabilidade

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
     <div>
-      <Header title="Hello Tairone" />
+      <Header title={ `Hello Tairone ${counter}` } />
       <h1>Conteudo da aplicação</h1>
+      <p>{ counter }</p>
+      <p>{ counter * 2 }</p>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
 }
