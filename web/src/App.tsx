82b677dc0474929964e3a78a599e8ff8208469ea
taironecdias <taironecdias@gmail.com>
2020-06-03 19:29:33 -0300

    aplicando conceitos de componentes e propriedades dentro do react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header title="Hello Tairone" />
+      <h1>Conteudo da aplicação</h1>
+    </div>
   );
 }
 
