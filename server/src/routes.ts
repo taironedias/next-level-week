8c97a38527a69c7eb6954ffb46f1bd8eb8b7c6a9
taironecdias <taironecdias@gmail.com>
2020-06-06 13:19:28 -0300

    adicionando o multer na rota de criação do ponto de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
@@ -13,8 +17,10 @@ routes.get('/', (req, res) => {
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', pointsController.create);
 routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
+
+
+routes.post('/points', upload.single('image'), pointsController.create);
 
 export default routes;
