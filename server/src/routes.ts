cecda9155bec93208ff2b26d1e776ce47fc30fde
taironecdias <taironecdias@gmail.com>
2020-06-06 16:13:15 -0300

    importando o celebrate e aplicando as regras de validação de cada campo

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
@@ -21,6 +22,24 @@ routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
 
 
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+    '/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create
+);
 
 export default routes;
