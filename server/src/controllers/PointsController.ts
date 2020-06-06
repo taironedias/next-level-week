27cadb056cec391bee98c7579ace574b0016aec6
taironecdias <taironecdias@gmail.com>
2020-06-06 13:24:24 -0300

    tratando o itenss, pois como passamos a enviar multipart form em vez de json, os ids dos itens foram enviados como string concatenados com vírgula

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -40,13 +40,16 @@ class PointsController {
         
         const point_id = insertedIds[0];
         
-        const pointItems = items.map((item_id: number) => {
-            return {
-                point_id,
-                item_id,
-            };
-        });
-        
+        const pointItems = items
+            .split(',')
+            .map((item: string) => Number(item.trim()))
+            .map((item_id: number) => {
+                return {
+                    point_id,
+                    item_id,
+                };
+            });
+
         await trx('point_items').insert(pointItems);
         
         await trx.commit();
